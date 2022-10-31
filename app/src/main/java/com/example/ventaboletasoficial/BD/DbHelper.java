b3e4960ce335b4c2f9619562f6f2d89da8be8527
Sala D10 <CSalaD10@uniboyaca.edu.co>
2022-10-31 04:31:32 -0500

    gg

--- app/src/main/java/com/example/ventaboletasoficial/BD/DbHelper.java
+++ app/src/main/java/com/example/ventaboletasoficial/BD/DbHelper.java
@@ -13,10 +13,46 @@ public class DbHelper extends SQLiteOpenHelper {
 
 
     private static final String CREAR_TABLA_USUARIOS = "CREATE TABLE " +
-            constantes.TABLA_USUARIO +
-            " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            constantes.TABLA_USUARIOS +
+            " (DOCUMENTO INTEGER PRIMARY KEY, " +
             " NOMBRE TEXT NOT NULL, " +
-            " EMAIL TEXT NOT NULL)";
+            " TELEFONO TEXT NOT NULL)";
+
+    private static final String CREAR_TABLA_VENTAS = "CREATE TABLE " +
+            constantes.TABLA_Ventas +
+            " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            " ASIENTOS TEXT NOT NULL, " +
+            " DOCUMENTOusu INTEGER," +
+            " TOTAL INTEGER,"+
+            " FOREIGN KEY (DOCUMENTOusu) REFERENCES "+constantes.TABLA_USUARIOS+ " (DOCUMENTO)"+
+            ")";
+
+    /*-- Employee definition
+
+CREATE TABLE [Employee]
+(
+    [EmployeeId] INTEGER  NOT NULL,
+    [LastName] NVARCHAR(20)  NOT NULL,
+    [FirstName] NVARCHAR(20)  NOT NULL,
+    [Title] NVARCHAR(30),
+    [ReportsTo] INTEGER,
+    [BirthDate] DATETIME,
+    [HireDate] DATETIME,
+    [Address] NVARCHAR(70),
+    [City] NVARCHAR(40),
+    [State] NVARCHAR(40),
+    [Country] NVARCHAR(40),
+    [PostalCode] NVARCHAR(10),
+    [Phone] NVARCHAR(24),
+    [Fax] NVARCHAR(24),
+    [Email] NVARCHAR(60),
+    CONSTRAINT [PK_Employee] PRIMARY KEY  ([EmployeeId]),
+    FOREIGN KEY ([ReportsTo]) REFERENCES [Employee] ([EmployeeId])
+		ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+CREATE UNIQUE INDEX [IPK_Employee] ON [Employee]([EmployeeId]);
+CREATE INDEX [IFK_EmployeeReportsTo] ON [Employee] ([ReportsTo]);*/
 
     public DbHelper(@Nullable Context context) {
         super(context, constantes.NOMBRE_BD, null, constantes.VERSIO_BD);
@@ -27,12 +63,13 @@ public class DbHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         //Creación tabla usuarios
         sqLiteDatabase.execSQL(CREAR_TABLA_USUARIOS);
+        sqLiteDatabase.execSQL(CREAR_TABLA_VENTAS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         //Actualiza de la BD por cambio de version
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+constantes.TABLA_USUARIO);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+constantes.TABLA_USUARIOS);
         sqLiteDatabase.execSQL(CREAR_TABLA_USUARIOS);
     }
 }
--- app/src/main/java/com/example/ventaboletasoficial/BD/constantes.java
+++ app/src/main/java/com/example/ventaboletasoficial/BD/constantes.java
@@ -3,5 +3,6 @@ package com.example.ventaboletasoficial.BD;
 public class constantes {
     public static final String NOMBRE_BD="RESERVA.db";
     public static final int VERSIO_BD=1;
-    public static final String TABLA_USUARIO="USUARIOS";
+    public static final String TABLA_USUARIOS="USUARIOS";
+    public static final String TABLA_Ventas="VENTAS";
 }
