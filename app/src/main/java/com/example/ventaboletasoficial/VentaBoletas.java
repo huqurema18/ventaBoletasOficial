d7731ace3ef48d4e1c77513bee3edb45f5bf3f96
nirttson <stevenorduz@hotmail.com>
2022-10-31 08:54:12 -0500

    v3

--- app/src/main/java/com/example/ventaboletasoficial/VentaBoletas.java
+++ app/src/main/java/com/example/ventaboletasoficial/VentaBoletas.java
@@ -28,39 +28,34 @@ public class VentaBoletas extends AppCompatActivity {
         txtNumAsientos = findViewById(R.id.txtNumAsientos);
 
         //Regresar
-        btnRegresar.setOnClickListener(v->{
+        btnRegresar.setOnClickListener(v -> {
             //Regresa al Intent principal
-            startActivity(new Intent(this,MainActivity.class));
+            startActivity(new Intent(this, MainActivity.class));
         });
 
         //Registra compra
-        btnComprar.setOnClickListener(v->{
-            try{
+        btnComprar.setOnClickListener(v -> {
+            try {
                 //Registra la venta al usuario
-                if(insertarVenta()){
+                if (registrarVentaBD()) {
                     //Confirma y envía a la actividad de reserva
                     Toast.makeText(this, "Registro de venta creado correctamente", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(this, VentaBoletas.class);
-                    intent.putExtra("documento",txtNumAsientos.getText());
+                    intent.putExtra("documento", txtNumAsientos.getText());
                     startActivity(intent);
-                }
-                else {
+                } else {
                     Toast.makeText(this, "No fue posible guardar la venta, por favor intente nuevamente", Toast.LENGTH_LONG).show();
                 }
-            }
-            catch  (Exception e){
+            } catch (Exception e) {
                 Toast.makeText(this, "Error, no fue posible insertar la venta", Toast.LENGTH_SHORT).show();
             }
         });
     }
-    private boolean insertarVenta(){
-        //Construir código para insertar registro y retornar
-        //True: registro insertado, false en cualquier otro caso
 
-        return true;
-    }
 
-    private void registrarVentaBD() {
+
+    private Boolean registrarVentaBD() {
+        Boolean query = false;
         try {
             //Conexion a la BD
             DbHelper dbHelper=new DbHelper(this);
@@ -68,19 +63,26 @@ public class VentaBoletas extends AppCompatActivity {
             SQLiteDatabase datos =dbHelper.getWritableDatabase();
             //permite hacer insercion al combinar clave-valor
             ContentValues values=new ContentValues();
-            //values.put("NOMBRE", txNombre.getText().toString());
-            //values.put("EMAIL", txEmail.getText().toString());
+            values.put("ASIENTOS", txtNumAsientos.getText().toString());
+            values.put("DOCUMENTOusu", txtNumAsientos.getText().toString());
+            values.put("TOTAL", txtNumAsientos.getText().toString());
 
             //Inserta el registro y retorna el ID
-            long id=datos.insert(constantes.TABLA_USUARIOS,null, values);
+            long id=datos.insert(constantes.TABLA_Ventas,null, values);
 
             if(id>0){
-                Toast.makeText(this, "USUARIO CREADO", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "VENTA CREADA", Toast.LENGTH_SHORT).show();
+                query=true;
             }else{
-                Toast.makeText(this, "ERROR AL CREAR EL USUARIO", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "ERROR AL CREAR LA VENTA", Toast.LENGTH_SHORT).show();
             }
         }catch (Exception e){
-            Toast.makeText(this, "NO FUE POSIBLE REGISTRAR LA INFORMACIÓN", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "NO FUE POSIBLE REGISTRAR LA VENTA", Toast.LENGTH_SHORT).show();
         }
+        return query;
+    }
+
+    private void operacionTotal() {
+
     }
 }
