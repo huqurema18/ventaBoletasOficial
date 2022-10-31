7d060d1592549106ef5d5d814037531946f9a0a1
Sala D10 <CSalaD10@uniboyaca.edu.co>
2022-10-31 04:35:11 -0500

    ggj

--- app/src/main/java/com/example/ventaboletasoficial/MainActivity.java
+++ app/src/main/java/com/example/ventaboletasoficial/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.ventaboletasoficial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.ventaboletasoficial.BD.DbHelper;
 
 public class MainActivity extends AppCompatActivity {
     Button btnIniciarSesion;
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         //Relación de objetos con Layout
         txtDocumento = findViewById(R.id.txtNumDocumento);
         btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+        
+        consultarUsuario();
 
         //Iniciar sesión
         btnIniciarSesion.setOnClickListener(v->{
@@ -46,6 +51,31 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    private void consultarUsuario() {
+        try {
+            //conexión
+            DbHelper dbHelper = new DbHelper(this);
+            //objeto para la lectura en la base de datos
+            SQLiteDatabase base_datos = dbHelper.getReadableDatabase();
+            //Arreglo con las condiciones de búsqueda, WHERE
+            /* String[] parametrosConsulta = {WEmail.getText().toString()};
+            //Arreglo con los camps a consultar, SELECT
+            String[] camposConsulta = {"NOMBRE"};
+            //Se define cursor para almacenar el resultado de la búsqueda
+            Cursor cursor = base_datos.query(Constantes.TABLA_USUARIO, camposConsulta, "EMAIL" + "=?",
+                    parametrosConsulta, null, null, null);
+            cursor.moveToFirst();
+            Toast.makeText(this, "Bienvenido " + cursor.getString(0), Toast.LENGTH_SHORT).show();
+
+            cursor.close();
+        } catch (Exception e) {
+            Toast.makeText(this, "Error al consultar Usuario", Toast.LENGTH_SHORT).show();
+            WEmail.setText("");
+        }*/
+    }catch (Exception e){}
+    }
+
     private boolean autenticarUsuario(){
         //Construir código para validar usuario y retornar
         //True: registro encontrado, false en cualquier otro caso
